4d26abe7805cf63fdce237ea736eb418d7ec5b39
Sergei Orlov <sorlov+github@gmail.com>
2019-05-10 22:56:40 -0700

    Graph now persists min/max dates until refresh

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,36 @@
 import { combineReducers } from 'redux';
 
+/*
+ * The min/max dates from all the different data sets we've graphed.
+ *
+ * We don't want to have the x-axis changing every time you select a filter,
+ * especially in the case that new events were added, or old events stopped
+ * being used.
+ *
+ * So we store these min/max-dates to ensure that if a new event was added, we
+ * see a bunch of 0-value dates in the graph, followed by an uptick when the
+ * event is added.  And if an event was phased out, we should see some 0-value
+ * dates on the right side of the x-axis.
+ */
+let globalMinDataDate;
+let globalMaxDataDate;
+
 function filteredEvents(state = {}, action) {
   if (action.type === 'GOT_FILTERED_EVENTS') {
+    if (action.value.data && action.value.data[0]) {
+      const minDataDate = action.value.data[0].date
+      const maxDataDate = action.value.data[action.value.data.length - 1].date
+      if (!globalMinDataDate || (minDataDate < globalMinDataDate)) {
+        globalMinDataDate = minDataDate
+      } else {
+        action.value.data.unshift({count: 0, date: globalMinDataDate})
+      }
+      if (!globalMaxDataDate || (maxDataDate > globalMaxDataDate)) {
+        globalMaxDataDate = maxDataDate
+      } else {
+        action.value.data.push({count: 0, date: globalMaxDataDate})
+      }
+    }
     return action.value;
   } else {
     return state;
